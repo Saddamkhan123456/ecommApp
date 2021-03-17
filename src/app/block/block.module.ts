b72dbb74fffad48d7204e8a9d23fff0dc07b77c1
Saddamkhan12346 <khansaddam444@gmail.com>
2021-03-17 17:20:18 +0530

    feature module

--- src/app/block/block.module.ts
+++ src/app/block/block.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
+import { BlockRoutingModule } from './block-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    BlockRoutingModule
   ]
 })
-export class AuthModule { }
+export class BlockModule { }
